96a3f03e10644acefea215806ea2edef67f372e5
Cat26610 <17322728964@163.com>
2026-05-03 02:53:42 +0800

    Site updated: 2026-05-03 02:53:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/30/1/","2026/04/29/网站重置记录/"];function toRandomPost(){
+var posts=["2026/04/29/网站重置记录/","2026/04/30/1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
