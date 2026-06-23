c66c41462944586e466c8cff52f028fde9127e18
Cat26610 <17322728964@163.com>
2026-06-23 14:28:15 +0800

    Site updated: 2026-06-23 14:28:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/23/3/","2026/04/30/1/","2026/04/29/网站重置记录/","2026/05/19/微观经济学笔记/"];function toRandomPost(){
+var posts=["2026/04/30/1/","2026/06/23/3/","2026/05/19/微观经济学笔记/","2026/04/29/网站重置记录/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
